b2c010227fdd6b93b583f2d7cd81ce2ba28ab3e3
Dimagio <sdmitriy3758@gmail.com>
2017-11-04 03:04:51 +0200

    Initial commit

---
+++ app/src/main/java/com/testapp/testapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.testapp.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_search){
+            //Todo launch finding query via retrofit
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
