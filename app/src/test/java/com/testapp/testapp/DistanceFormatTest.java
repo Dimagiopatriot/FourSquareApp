72cf114afa466056547cb6a75503b46bdba6e73c
Dimagio <sdmitriy3758@gmail.com>
2017-11-06 02:18:24 +0200

    Test for utils

---
+++ app/src/test/java/com/testapp/testapp/DistanceFormatTest.java
@@ -0,0 +1,27 @@
+package com.testapp.testapp;
+
+import com.testapp.testapp.model.utils.DistanceFormatter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+/**
+ * Created by troll on 05.11.2017.
+ */
+
+public class DistanceFormatTest {
+
+    @Test
+    public void fromMetersToKilometersUpperRoundTest(){
+        String expected = "6.4";
+        String actual = DistanceFormatter.fromMetersToKilometersFormat(6352);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void fromMetersToKilometersLowerRoundTest(){
+        String expected = "6.3";
+        String actual = DistanceFormatter.fromMetersToKilometersFormat(6348);
+        assertEquals(expected, actual);
+    }
+}
---
+++ app/src/test/java/com/testapp/testapp/SizeChangerTest.java
@@ -0,0 +1,44 @@
+package com.testapp.testapp;
+
+import com.testapp.testapp.model.utils.Constants;
+import com.testapp.testapp.model.utils.SizeChanger;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+/**
+ * Created by troll on 05.11.2017.
+ */
+
+public class SizeChangerTest {
+
+
+    @Test
+    public void zoomInUpperRoundTest(){
+        int expected = 5860;
+        int actual = SizeChanger.zoomIn(586, Constants.SCALE);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void zoomInLowerRoundTest(){
+        int expected = 5940;
+        int actual = SizeChanger.zoomIn(594, Constants.SCALE);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void zoomOutUpperRoundTest(){
+        int expected = 59;
+        int actual = SizeChanger.zoomOut(589, Constants.SCALE);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void zoomOutLowerRoundTest(){
+        int expected = 59;
+        int actual = SizeChanger.zoomOut(594, Constants.SCALE);
+        assertEquals(expected, actual);
+    }
+
+}
