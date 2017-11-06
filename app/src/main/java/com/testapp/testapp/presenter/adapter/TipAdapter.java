3b7582158bb1345a354522a947c8a9fe886e0e58
Dimagio <sdmitriy3758@gmail.com>
2017-11-06 02:18:55 +0200

    Add adapters for recycler views

---
+++ app/src/main/java/com/testapp/testapp/presenter/adapter/TipAdapter.java
@@ -0,0 +1,79 @@
+package com.testapp.testapp.presenter.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.testapp.testapp.R;
+import com.testapp.testapp.model.entity.Tip;
+
+import java.util.List;
+
+/**
+ * Created by troll on 06.11.2017.
+ */
+
+public class TipAdapter extends RecyclerView.Adapter<TipAdapter.Holder> {
+
+    private List<Tip> tips;
+
+    public void addVenues(List<Tip> tips){
+        this.tips.addAll(tips);
+        notifyDataSetChanged();
+    }
+
+    public void clearVenues(){
+        tips.clear();
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View row = LayoutInflater.from(parent.getContext()).inflate(R.layout.tip_item_row, parent,false);
+        return new Holder(row);
+    }
+
+    @Override
+    public void onBindViewHolder(Holder holder, int position) {
+        Tip tip = tips.get(position);
+
+        holder.text.setText(tip.getTipText());
+        holder.likes.setText(tip.getTipLikes().getCount());
+        holder.userName.setText(tip.getUser().getUserFirstName());
+
+        Context context = holder.itemView.getContext();
+        Picasso.with(context).load(tip.getPhoto().toString()).into(holder.image);
+        Picasso.with(context).load(tip.getUser().getUserPhoto().toString()).into(holder.image);
+    }
+
+    @Override
+    public int getItemCount() {
+        return tips.size();
+    }
+
+    class Holder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        ImageView image, userAvatar;
+        TextView text, userName, likes;
+
+        public Holder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+            image = (ImageView) itemView.findViewById(R.id.tipImage);
+            userAvatar = (ImageView) itemView.findViewById(R.id.tipUserIcon);
+            text = (TextView) itemView.findViewById(R.id.tipText);
+            userName = (TextView) itemView.findViewById(R.id.tipUserName);
+            likes = (TextView) itemView.findViewById(R.id.tipLikes);
+        }
+
+        @Override
+        public void onClick(View v) {
+            //Nothing to do
+        }
+    }
+}
---
+++ app/src/main/java/com/testapp/testapp/presenter/adapter/VenueAdapter.java
@@ -0,0 +1,106 @@
+package com.testapp.testapp.presenter.adapter;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.testapp.testapp.R;
+import com.testapp.testapp.VenueDetailsActivity;
+import com.testapp.testapp.model.entity.Photo;
+import com.testapp.testapp.model.entity.Venue;
+import com.testapp.testapp.model.utils.DistanceFormatter;
+
+import java.util.List;
+
+/**
+ * Created by troll on 05.11.2017.
+ */
+
+public class VenueAdapter extends RecyclerView.Adapter<VenueAdapter.Holder> {
+
+    private List<Venue> venues;
+
+    public void addVenues(List<Venue> venues){
+        this.venues.addAll(venues);
+        notifyDataSetChanged();
+    }
+
+    public void clearVenues(){
+        venues.clear();
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View row = LayoutInflater.from(parent.getContext()).inflate(R.layout.venue_item_row, parent, false);
+        return new Holder(row);
+    }
+
+    @Override
+    public void onBindViewHolder(Holder holder, int position) {
+        Venue currentVenue = venues.get(position);
+
+        holder.name.setText(currentVenue.getName());
+        holder.category.setText(currentVenue.getPrimaryCategory().getName());
+        holder.address.setText(currentVenue.getLocation().getAddress());
+        holder.price.setText(currentVenue.getPrice().toString());
+        holder.distance.setText(DistanceFormatter.fromMetersToKilometersFormat(currentVenue.getLocation().getDistance()));
+
+        holder.rating.setText(String.valueOf(currentVenue.getRating()));
+        holder.rating.setBackgroundColor(Color.parseColor("#" + currentVenue.getRatingColor()));
+        checkVenuePhotos(holder, currentVenue);
+    }
+
+    private void checkVenuePhotos(Holder holder, Venue venue){
+        if (venue.getPhotoWrapper() == null){
+            setVenueImage(holder, venue.getPrimaryCategory().getCategoryIcon().toString());
+        } else {
+            Photo venuePhoto = venue.getPhotoWrapper().getItems().get(0);
+            if (venuePhoto != null){
+                venuePhoto.zoomOutWidth();
+                venuePhoto.zoomOutHeiht();
+                setVenueImage(holder, venuePhoto.toString());
+            }
+        }
+    }
+
+    private void setVenueImage(Holder holder, String url){
+        Picasso.with(holder.itemView.getContext()).load(url).into(holder.venueImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return venues.size();
+    }
+
+    class Holder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        ImageView venueImage;
+
+        TextView name, category, address, price, distance, rating;
+
+        public Holder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+            venueImage = (ImageView) itemView.findViewById(R.id.venueImage);
+            name = (TextView) itemView.findViewById(R.id.venueName);
+            category = (TextView) itemView.findViewById(R.id.venueCategory);
+            address = (TextView) itemView.findViewById(R.id.venueAddress);
+            price = (TextView) itemView.findViewById(R.id.venuePrice);
+            distance = (TextView) itemView.findViewById(R.id.venueDistance);
+            rating = (TextView) itemView.findViewById(R.id.venueRating);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(v.getContext(), VenueDetailsActivity.class);
+            v.getContext().startActivity(intent);
+        }
+    }
+}
